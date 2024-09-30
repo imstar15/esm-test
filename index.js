fe9b68d7a704a336466021384bd143e51978375f
imstar15 <imstar15@qq.com>
2024-10-01 03:14:04 +0800

    Add dynamic import

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import { Mangata } from '@mangata-finance/sdk';
 
 console.log('Keyring: ', Keyring);
 console.log('Mangata: ', Mangata);
+
+// // Dynamic import
+// (async () => {
+//   const DynacicMangataSDK = await import('@mangata-finance/sdk');
+//   console.log('DynacicMangataSDK: ', DynacicMangataSDK);
+// })();
